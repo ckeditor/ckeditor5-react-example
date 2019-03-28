5b62baa094d9c891cf11bb2254723815ab0eb472
Piotrek Koszuliński <pkoszulinski@gmail.com>
2019-03-28 14:55:43 +0100

    Used CKEditor 5 component.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
+
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import CKEditor from '@ckeditor/ckeditor5-react';
+
+// NOTE: We use editor from source (not a build)!
+import ClassicEditor from '@ckeditor/ckeditor5-editor-classic/src/classiceditor';
+
+import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials';
+import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold';
+import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic';
+import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph';
+
+const editorConfiguration = {
+  plugins: [ Essentials, Bold, Italic, Paragraph ],
+  toolbar: [ 'bold', 'italic' ]
+};
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h2>Using CKEditor 5 from source in React</h2>
+        <CKEditor
+          editor={ ClassicEditor }
+          config={ editorConfiguration }
+          data="<p>Hello from CKEditor 5!</p>"
+          onInit={ editor => {
+            // You can store the "editor" and use when it is needed.
+            console.log( 'Editor is ready to use!', editor );
+          } }
+          onChange={ ( event, editor ) => {
+            const data = editor.getData();
+            console.log( { event, editor, data } );
+          } }
+          onBlur={ editor => {
+            console.log( 'Blur.', editor );
+          } }
+          onFocus={ editor => {
+            console.log( 'Focus.', editor );
+          } }
+        />
       </div>
     );
   }
