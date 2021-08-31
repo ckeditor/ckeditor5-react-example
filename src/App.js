bcb6f68d551045fb881ab4381d729749ecc3681a
Mihai Coloji <mihai.coloji@aeratechnology.com>
2021-08-31 18:28:34 +0300

    feat: add ckeditor-mention module + simple example

--- src/App.js
+++ src/App.js
@@ -10,11 +10,62 @@ import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials';
 import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold';
 import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic';
 import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph';
+import Mention from '@ckeditor/ckeditor5-mention/src/mention';
 
+// original working config
+// const editorConfiguration = {
+//   plugins: [ Essentials, Bold, Italic, Paragraph ],
+//   toolbar: [ 'bold', 'italic' ]
+// };
+
+// modified config with mentions
 const editorConfiguration = {
-  plugins: [ Essentials, Bold, Italic, Paragraph ],
-  toolbar: [ 'bold', 'italic' ]
+  plugins: [Mention, Essentials, Bold, Italic, Paragraph],
+  mention: {
+    feeds: [{
+      marker: '/',
+      feed: getFeedItems
+    }]
+  },
+  toolbar: ['bold', 'italic']
 };
+
+const items = [
+  { id: 'option1', userId: '1', name: 'Option1', link: 'https://www.imdb.com/title/tt0460649/characters/nm0000439' },
+  { id: 'option2', userId: '2', name: 'Option2', link: 'https://www.imdb.com/title/tt0460649/characters/nm0004989' },
+  { id: 'option3', userId: '3', name: 'Option3', link: 'https://www.imdb.com/title/tt0460649/characters/nm0781981' },
+  { id: 'option4', userId: '4', name: 'Option4', link: 'https://www.imdb.com/title/tt0460649/characters/nm1130627' },
+  { id: 'option5', userId: '5', name: 'Option5', link: 'https://www.imdb.com/title/tt0460649/characters/nm1102140' }
+];
+
+function getFeedItems( queryText ) {
+  // As an example of an asynchronous action, return a promise
+  // that resolves after a 100ms timeout.
+  // This can be a server request or any sort of delayed action.
+  return new Promise( resolve => {
+      setTimeout( () => {
+          const itemsToDisplay = items
+              // Filter out the full list of all items to only those matching the query text.
+              .filter( isItemMatching )
+              // Return 10 items max - needed for generic queries when the list may contain hundreds of elements.
+              .slice( 0, 10 );
+
+          resolve( itemsToDisplay );
+      }, 100 );
+  } );
+
+  // Filtering function - it uses the `name` and `username` properties of an item to find a match.
+  function isItemMatching( item ) {
+      // Make the search case-insensitive.
+      const searchString = queryText.toLowerCase();
+
+      // Include an item in the search results if the name or username includes the current user input.
+      return (
+          item.name.toLowerCase().includes( searchString ) ||
+          item.id.toLowerCase().includes( searchString )
+      );
+  }
+}
 
 class App extends Component {
   render() {
